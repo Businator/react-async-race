1e72484d22660fd3455e5b716b070eca1623ec2d
Marat Deniullov <99427650+Businator@users.noreply.github.com>
2023-03-03 17:32:03 +0400

    feat: add race all and reset all cars functions

--- src/components/Car/components/ActionButtons/ActionButtons.jsx
+++ src/components/Car/components/ActionButtons/ActionButtons.jsx
@@ -1,55 +1,7 @@
-import { useState } from 'react';
-import { drive, startEngine, stopEngine } from '../../../../api';
+import { startDriving, stopDriving } from '../../../../utils/workWithDriving';
 import styles from './ActionButtons.module.css';
 
 export const ActionButtons = ({ id }) => {
-  const getCar = (id) => {
-    return document.querySelector(`[datatype = 'car${id}']`);
-  };
-
-  const animationCar = (car, time) => {
-    const carWidth = car.getBoundingClientRect().width;
-    const animation = car.animate(
-      [{ left: '0%' }, { left: `calc(100% - ${carWidth}px)` }],
-      {
-        duration: time,
-        easing: 'ease-out',
-      }
-    );
-    animation.play();
-    animation.onfinish = () => {
-      car.style.left = `calc(100% - ${carWidth}px)`;
-    };
-  };
-
-  const switchToDriveMode = async (id) => {
-    const status = await drive(id);
-    if (status === 500) {
-      getCar(id)
-        .getAnimations({ subtree: false })
-        .map((anim) => anim.pause());
-    }
-  };
-
-  const startDriving = async (id) => {
-    const { status, result } = await startEngine(id);
-    if (status === 200) {
-      const time = result.distance / result.velocity;
-      switchToDriveMode(id);
-      animationCar(getCar(id), time);
-    }
-  };
-
-  const stopDriving = async (id) => {
-    const { status } = await stopEngine(id);
-    if (status === 200) {
-      getCar(id)
-        .getAnimations()
-        .map((anim) => anim.cancel());
-      getCar(id).style.left = '0%';
-    }
-  };
-
   return (
     <div className={styles.container}>
       <button onClick={async () => await startDriving(id)}>Start</button>
--- src/components/CarCreationMenu/components/ActionButtons/ActionButtons.jsx
+++ src/components/CarCreationMenu/components/ActionButtons/ActionButtons.jsx
@@ -1,6 +1,8 @@
 import { useContext } from 'react';
 import { CarContext } from '../../../../context/CarContext';
 import { generateRandomCars } from '../../../../utils/generateRandomCars';
+import { getCars } from '../../../../api';
+import { startDriving, stopDriving } from '../../../../utils/workWithDriving';
 
 export const ActionButtons = ({ setIsCreateNewCar }) => {
   const carContext = useContext(CarContext);
@@ -11,10 +13,22 @@ export const ActionButtons = ({ setIsCreateNewCar }) => {
     setIsCreateNewCar(true);
   };
 
+  const raceAllCars = async () => {
+    const { items } = await getCars(1);
+    const cars = items.map(async (car) => startDriving(car.id));
+    Promise.race(cars);
+  };
+
+  const resetAllcar = async () => {
+    const { items } = await getCars(1);
+    const cars = items.map(async (car) => stopDriving(car.id));
+    Promise.all(cars);
+  };
+
   return (
     <div>
-      <button>Race</button>
-      <button>Reset</button>
+      <button onClick={async () => await raceAllCars()}>Race</button>
+      <button onClick={async () => await resetAllcar()}>Reset</button>
       <button onClick={generateCars}>Generate Cars</button>
     </div>
   );
