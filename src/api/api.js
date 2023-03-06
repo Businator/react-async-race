bed21a9d3a917105c9b03fa2ef60ab737e81dc75
Marat Deniullov <99427650+Businator@users.noreply.github.com>
2023-03-06 12:10:10 +0400

    feat: add functions for work with winners in api

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,13 @@
 const base = 'http://127.0.0.1:3000';
+const garage = `${base}/garage`;
+const engine = `${base}/engine`;
+const winners = `${base}/winners`;
+
+///////////////Work With Cars///////////////
 
 export const getCars = async (page, limit = 7) => {
   try {
-    const response = await fetch(
-      `${base}/garage?_page=${page}&_limit=${limit}`
-    );
+    const response = await fetch(`${garage}?_page=${page}&_limit=${limit}`);
 
     return {
       items: await response.json(),
@@ -17,7 +20,7 @@ export const getCars = async (page, limit = 7) => {
 
 export const getCar = async (id) => {
   try {
-    const response = await fetch(`${base}/garage/${id}}`);
+    const response = await fetch(`${garage}/${id}}`);
     return { status: response.status, result: await response.json() };
   } catch (error) {
     throw new Error(error);
@@ -26,7 +29,7 @@ export const getCar = async (id) => {
 
 export const createCar = async (car) => {
   try {
-    await fetch(`${base}/garage`, {
+    await fetch(`${garage}`, {
       method: 'POST',
       body: JSON.stringify(car),
       headers: {
@@ -40,7 +43,7 @@ export const createCar = async (car) => {
 
 export const deleteCar = async (id) => {
   try {
-    await fetch(`${base}/garage/${id}`, {
+    await fetch(`${garage}/${id}`, {
       method: 'DELETE',
     });
   } catch (error) {
@@ -50,7 +53,7 @@ export const deleteCar = async (id) => {
 
 export const updateCar = async (id, car) => {
   try {
-    await fetch(`${base}/garage/${id}`, {
+    await fetch(`${garage}/${id}`, {
       method: 'PUT',
       body: JSON.stringify(car),
       headers: {
@@ -62,9 +65,11 @@ export const updateCar = async (id, car) => {
   }
 };
 
+///////////////Work With Engine///////////////
+
 export const startEngine = async (id) => {
   try {
-    const response = await fetch(`${base}/engine?id=${id}&status=started`, {
+    const response = await fetch(`${engine}?id=${id}&status=started`, {
       method: 'PATCH',
     });
     return {
@@ -78,7 +83,7 @@ export const startEngine = async (id) => {
 
 export const stopEngine = async (id) => {
   try {
-    const response = await fetch(`${base}/engine?id=${id}&status=stopped`, {
+    const response = await fetch(`${engine}?id=${id}&status=stopped`, {
       method: 'PATCH',
     });
     return {
@@ -92,11 +97,80 @@ export const stopEngine = async (id) => {
 
 export const drive = async (id) => {
   try {
-    const response = await fetch(`${base}/engine?id=${id}&status=drive`, {
+    const response = await fetch(`${engine}?id=${id}&status=drive`, {
       method: 'PATCH',
     });
     return response.status;
   } catch (error) {
     throw new Error(error);
+  }
+};
+
+///////////////Work With Winners///////////////
+
+export const getWinners = async (
+  page,
+  limit = 10,
+  sort = 'time',
+  order = 'ASC'
+) => {
+  try {
+    const response = await fetch(
+      `${winners}?_page=${page}&_limit=${limit}&_sort=${sort}&_order=${order}`
+    );
+    return {
+      result: await response.json(),
+      totalCount: response.headers.get('X-Total-Count') || '0',
+    };
+  } catch {
+    throw new Error();
+  }
+};
+
+export const getWinner = async (id) => {
+  try {
+    const response = await fetch(`${winners}/${id}`);
+    return {
+      status: response.status,
+      result: await response.json(),
+    };
+  } catch {
+    throw new Error();
+  }
+};
+
+export const createWinner = async (car) => {
+  try {
+    const response = await fetch(`${winners}`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(car),
+    });
+
+    return response.status;
+  } catch {
+    throw new Error();
+  }
+};
+
+export const deleteWinner = async (id) => {
+  try {
+    await fetch(`${winners}/${id}`, {
+      method: 'DELETE',
+    });
+  } catch {
+    throw new Error();
+  }
+};
+
+export const updateWinner = async (car) => {
+  try {
+    await fetch(`${winners}/${car.id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(car),
+    });
+  } catch {
+    throw new Error();
   }
 };
